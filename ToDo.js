d7e305f4d0eb60f9ca7a28966fed250417e5b595
Adasusss <A.Bartoszko@wp.pl>
2018-05-08 22:05:43 +0200

    First

---
+++ ToDo.js
@@ -0,0 +1,32 @@
+//check off 
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("crossed");
+})
+
+//delete
+$("ul").on("click", "span", function(event){
+    event.stopPropagation(); //zatrzymuje event (w tym wypadku nadanie klasy crossed)
+    $(this).parent().fadeOut(500, function(){
+    $(this).remove(); //usuwa li
+    })
+})
+
+//delete whole list
+$(".fa-trash").click(function(){
+    $("ul").fadeOut(500, function(){
+        $("li").remove();
+    })
+    $("ul").fadeIn();
+})
+
+//add
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13 && $(this).val() !== "" && $(this).val().length < 30){
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + $(this).val() + "</li>");
+        $(this).val("");
+    }
+})
+//toggle input
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+})
